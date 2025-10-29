7e619709d4cf2bf0ff8c6acb041e5a2734ca95c6
Shreya Karka <shreyakarka@gmail.com>
2025-10-29 12:17:55 -0500

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Shreya-Karka/dsa-heatmap-hackerrank", // Change this to your repo name
+  base: "/dsa-heatmap-hackerrank/", // Change this to your repo name
   build: {
     outDir: "dist",
     assetsDir: "assets",
